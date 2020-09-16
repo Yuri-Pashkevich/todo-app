5afee32802687b24eb66c0a9656eb98982f0e7cb
yuri_pashkevich <shmulhe@gmail.com>
2020-09-16 18:40:21 +0300

    Updates

---
+++ precache-manifest.ef2e5765f5ca9c90b4e8f8d8f360e5e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b916809eaf7148f96f7388a6336db76",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "1e0d5fcde456575fbe8a",
+    "url": "/todo-app/static/css/main.2e498fcd.chunk.css"
+  },
+  {
+    "revision": "3710d982c844cc4fe830",
+    "url": "/todo-app/static/js/2.1a9cc29b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.1a9cc29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0d5fcde456575fbe8a",
+    "url": "/todo-app/static/js/main.dd450183.chunk.js"
+  },
+  {
+    "revision": "59a60f66a7179493024f",
+    "url": "/todo-app/static/js/runtime-main.f617a8c6.js"
+  }
+]);
